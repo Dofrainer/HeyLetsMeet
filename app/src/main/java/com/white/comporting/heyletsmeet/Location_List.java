c040ba3c792e2f0e10aa459917ba4f9da6d4d6ed
Dofrainer <Cwhite@outlook.kr>
2016-04-11 04:30:20 +0900

    location_select.xml 추가 Location_Select.java 추가   - 위치선택(임시) 기능 추가

--- app/src/main/java/com/white/comporting/heyletsmeet/Location_List.java
+++ app/src/main/java/com/white/comporting/heyletsmeet/Location_List.java
@@ -1,7 +1,11 @@
 package com.white.comporting.heyletsmeet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by Baek on 2016-04-11.
@@ -10,6 +14,21 @@ public class Location_List  extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.location_list);
+        findViewById(R.id.TextLocation).setOnClickListener(mClickListener);
+
 
     }
+    TextView.OnClickListener mClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.TextLocation:
+                    Log.d("OnClickListener", "click session button");
+                    // 액티비티 실행
+                    Intent intentSubActivity =
+                            new Intent(Location_List.this, Location_Select.class);
+                    startActivity(intentSubActivity);
+                    break;
+            }
+        }
+    };
 }
