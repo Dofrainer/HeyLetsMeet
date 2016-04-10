9d270f3c389a6142ae31f8d2387e5a34f8c2b04b
Dofrainer <Cwhite@outlook.kr>
2016-04-11 03:43:39 +0900

    레이아웃 순서대로 작동

---
+++ app/src/main/java/com/white/comporting/heyletsmeet/Location_List.java
@@ -0,0 +1,15 @@
+package com.white.comporting.heyletsmeet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Baek on 2016-04-11.
+ */
+public class Location_List  extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.location_list);
+
+    }
+}
--- app/src/main/java/com/white/comporting/heyletsmeet/MainActivity.java
+++ app/src/main/java/com/white/comporting/heyletsmeet/MainActivity.java
@@ -1,6 +1,9 @@
 package com.white.comporting.heyletsmeet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -8,6 +11,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.start);
+        Handler handler = new Handler(){
+            public void handleMessage(Message msg){
+                super.handleMessage(msg);
+                startActivity(new Intent(MainActivity.this, Location_List.class));
+                //overridePendingTransition(R.anim.abc_fade_in, 0);
+                finish();
+            }
+        };
+        handler.sendEmptyMessageDelayed(0,3000);
     }
 }
