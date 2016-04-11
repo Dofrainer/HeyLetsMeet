bac799ec8174c5243104420cd37afd201457f873
dofrainer <rlfwns2325@gmail.com>
2016-04-11 11:41:34 +0900

    미들 포지션 선택,lxml(가설)추가 미들 포지션 선택.java(가설 추가

---
+++ app/src/main/java/com/white/comporting/heyletsmeet/Select_Middle_Position.java
@@ -0,0 +1,28 @@
+package com.white.comporting.heyletsmeet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Baek on 2016-04-11.
+ */
+public class Select_Middle_Position extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_middle_position);
+        findViewById(R.id.btnEndSelect).setOnClickListener(mClickListener);
+
+    }
+    Button.OnClickListener mClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btnEndSelect:
+                    // 액티비티 실행
+                    finish();
+                    break;
+            }
+        }
+    };
+}
