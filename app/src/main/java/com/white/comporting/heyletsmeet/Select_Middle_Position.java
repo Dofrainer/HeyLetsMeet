d14b5e3ccd8e37fcb429160369f26aab4afa3392
dofrainer <rlfwns2325@gmail.com>
2016-04-11 12:00:07 +0900

    select middle position 의 선택버튼에 이벤트 추가

--- app/src/main/java/com/white/comporting/heyletsmeet/Select_Middle_Position.java
+++ app/src/main/java/com/white/comporting/heyletsmeet/Select_Middle_Position.java
@@ -12,13 +12,13 @@ public class Select_Middle_Position extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_middle_position);
-        findViewById(R.id.btnEndSelect).setOnClickListener(mClickListener);
+        findViewById(R.id.Btn_Select_Middle).setOnClickListener(mClickListener);
 
     }
     Button.OnClickListener mClickListener = new View.OnClickListener() {
         public void onClick(View v) {
             switch (v.getId()) {
-                case R.id.btnEndSelect:
+                case R.id.Btn_Select_Middle:
                     // 액티비티 실행
                     finish();
                     break;
