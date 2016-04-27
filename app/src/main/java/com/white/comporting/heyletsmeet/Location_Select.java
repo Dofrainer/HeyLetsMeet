8302ad915c97bdc15e0716487391dc2dee6f45bb
dofrainer <rlfwns2325@gmail.com>
2016-04-27 20:09:00 +0900

    Select Location 위치 선택 네이버 지도 추가 (미완)

--- app/src/main/java/com/white/comporting/heyletsmeet/Location_Select.java
+++ app/src/main/java/com/white/comporting/heyletsmeet/Location_Select.java
@@ -1,18 +1,43 @@
 package com.white.comporting.heyletsmeet;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.nhn.android.maps.NMapActivity;
+import com.nhn.android.maps.NMapController;
+import com.nhn.android.maps.NMapView;
 
 /**
  * Created by Baek on 2016-04-11.
  */
-public class Location_Select  extends AppCompatActivity {
+public class Location_Select  extends NMapActivity{
+    private NMapView mMapView;
+    private NMapController mMapController;
+
+    String ApiId = "Rl8S6gGJny8y5aRlpCbd";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.location_select);
+
+        mMapView = (NMapView)findViewById(R.id.mapView);
+        mMapView.setClientId(ApiId);
+
+        mMapView.setEnabled(true);
+        mMapView.setFocusable(true);
+        mMapView.setFocusableInTouchMode(true);
+        mMapView.requestFocus();
+
+        mMapController = mMapView.getMapController();
+        NMapView.LayoutParams lp = new NMapView.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT, NMapView.LayoutParams.BOTTOM_RIGHT);
+        mMapView.setBuiltInZoomControls(true, lp);
+
+
+
         findViewById(R.id.btnEndSelect).setOnClickListener(mClickListener);
+
 
     }
     Button.OnClickListener mClickListener = new View.OnClickListener() {
@@ -25,4 +50,6 @@ public class Location_Select  extends AppCompatActivity {
             }
         }
     };
+
+
 }
